346e2faaf50dd191496415839a3e8770bf3fee0e
Nightshade14 <satyamchatrola14@gmail.com>
2024-11-10 02:47:44 -0500

    debug: Test circleCI testing with intentional failing tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ def test_addition() -> None:
     """Test the addition function."""
     test_7 = 7
     test_0 = 0
-    test__2 = -2
+    test__2 = -22
     assert addition(3, 4) == test_7
     assert addition(-1, 1) == test_0
     assert addition(-1, -1) == test__2
@@ -15,7 +15,7 @@ def test_addition() -> None:
 
 def test_substraction() -> None:
     """Test the addition function."""
-    test_7 = 7
+    test_7 = 71
     test_0 = 0
     test_negative_2 = -2
     assert substraction(10, 3) == test_7
